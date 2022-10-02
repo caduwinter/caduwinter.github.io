da3d489a92c86853b1779b5393457d6c1410152a
caduwinter <104656553+caduwinter@users.noreply.github.com>
2022-10-02 20:03:17 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function extendLi(){
+    document.createElement();
+}
+
+function toggleLi(){
+
+}
+
+const button = document.getElementById('button');
+console.log(button);
+
+button.addEventListener('click', extendLi);
