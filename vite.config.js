2a59798dc5af148ac0905672510562db673fc690
carloswinter <caduwinteer@gmail.com>
2023-03-10 02:20:22 -0300

    add

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import vitePluginString from "vite-plugin-string";
+import glsl from "vite-plugin-glsl";
+import { defineConfig } from "vite";
 
-export default {
-  plugins: [vitePluginString()],
-};
+export default defineConfig({
+  plugins: [vitePluginString(), glsl()],
+});
